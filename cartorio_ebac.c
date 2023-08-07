1d6a4cf79d1d64f2bc442b02cb22f24614b4f6cf
Lucasnscto <141625983+Lucasnscto@users.noreply.github.com>
2023-08-07 06:29:53 -0300

    Opção de saída

--- cartorio_ebac.c
+++ cartorio_ebac.c
@@ -129,7 +129,8 @@ int main()
         printf("\t1 - Registrar nomes\n");
         printf("\t2 - Consultar nomes\n");
         printf("\t3 - Deletar nomes\n\n"); 
-	    printf("Opção: ");//fim do menu
+		printf("\t4 - Sair do sistema\n\n");
+		printf("Opção: ");//fim do menu
     
         scanf("%d", &opcao); //armazenando a escolha do usuário
 	
@@ -150,7 +151,13 @@ int main()
             deletar();
 		    break;
 		    
-		    default:
+		    case 4:
+		    printf("Obrigado por utilizar o sistema!\n");
+		    return 0;
+		    break;
+			
+			
+			default:
 		    printf("Essa opção não está disponível!\n");
 	        system("pause");
 	        break;
